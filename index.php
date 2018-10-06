1cd1ad6bbabe7afb488757ae6e0f622dfb865e87
miyasatotakafumi <miyasato@saikaicreative.co.jp>
2018-10-06 17:14:50 +0900

    バージョン２

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>ブックマーク</title>
+  <link rel="stylesheet" href="css/main.css">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body>
+
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+    <div class="navbar-header"><a class="navbar-brand" href="select.php">ブックマーク</a></div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<form method="post" action="insert.php">
+  <div class="jumbotron">
+   <fieldset>
+    <legend>ブックマーク記事</legend>
+     <label>書籍名<input type="text" name="name"></label><br>
+     <label>書籍URL<input type="text" name="url"></label><br>
+     <label><textArea name="comment" rows="4" cols="40" placeholder="コメント"></textArea></label><br>
+     <input type="submit" value="送信">
+    </fieldset>
+  </div>
+</form>
+<!-- Main[End] -->
+
+
+</body>
+</html>
---
+++ insert.php
@@ -0,0 +1,45 @@
+<?php
+//そもそもデータあるか確認
+if(
+  !isset($_POST["name"]) || $_POST["name"]=="" ||
+  !isset($_POST["url"]) || $_POST["url"]=="" ||
+  !isset($_POST["comment"]) || $_POST["comment"]==""
+){
+  exit('ParamError');
+}
+
+//1. POSTデータ取得
+//$name = filter_input( INPUT_GET, "name" ); //こういうのもあるよ
+//$email = filter_input( INPUT_POST, "email" ); //こういうのもあるよ
+$name = $_POST["name"];
+$url = $_POST["url"];
+$comment = $_POST["comment"];
+
+//2. DB接続します (注意！！データベース名！テーブル名ではない！！)
+try {
+  $pdo = new PDO('mysql:dbname=gs_f01_db30;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) {
+  exit('dbError:'.$e->getMessage());
+}
+
+//３．データ登録SQL作成
+$sql ="INSERT INTO gs_bm_table(id,name,url,comment,datetime)
+VALUES(NULL,:a1,:a2,:a3,sysdate())";
+
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':a1', $name, PDO::PARAM_STR);    //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':a2', $url, PDO::PARAM_STR);   //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':a3', $comment, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$status = $stmt->execute();
+
+//４．データ登録処理後
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("sqlError:".$error[2]); //二番目だけ、理解できるものが帰ってくる
+}else{
+  //５．index.phpへリダイレクト
+  header("location: index.php");
+
+}
+?>
